52bbf9dbf7acbc1599480b8c54d691df7e5130ab
ShadowBurn1210 <leo.saulitis@gmail.com>
2024-03-19 21:09:13 +0200

    Works with multiple digits and make result double

--- Homeworks/Week9/arithmetic_parser.cpp
+++ Homeworks/Week9/arithmetic_parser.cpp
@@ -17,9 +17,14 @@ int main(){
     char prev = ' ';
     bool last = false;
     int count = 0;
-    for (char i : input){
+    for (int index = 0; index < input.size(); ++index){
+
+        // I needed this line because all the project was build with for (char i : input) this line
+        // However, when I added numbers with more than one digit, I had to change it to for (int index = 0; index < input.size(); ++index)
+        char i = input[index];
         count++;
 
+        // all the error handeling
         if (is_letter(i)){
             cout << "Invalid input" << endl;
             exit(7);
@@ -41,25 +46,32 @@ int main(){
             last = false;
         }
         try {
+            // check that character is not a number
             if (i < '0' || i > '9'){
 
+                // is character is a bracket handle it separately
                 if (is_bracket(i)){
                     if (i == '(')
                     {
                         operators.push(i);
+                        operators.push(' ');
                     }
                     else if (i == ')')
                     {
                         while (!operators.empty() && operators.top() != '(')
                         {
                             outputs.push(operators.top()) ;
+                            outputs.push(' ');
+
                             operators.pop();
                         }
                         operators.pop();
                     }
                 }
 
+                // if the character is an operator
                 if (isOperator(i)) {
+                    // throw an error if the previous character is an operator and the current character is not a bracket
                     if (isOperator(prev) && i != '(' && i != ')'){
                         cout << "Invalid input" << endl;
                         exit(8);
@@ -67,26 +79,39 @@ int main(){
                         prev = i;
                     }
 
-
+                    // if the stack is empty, push the operator to the stack
                     if (operators.empty()) {
                         operators.push(i);
+
                         continue;
                     }
+
+                    // if the stack is not empty, check the priority of the operator
                     int top_precident = priotity(operators.top());
                     int current_precident = priotity(i);
+
+                    // if the current operator has a higher priority than the top operator, push the current operator to the stack
                     if (current_precident > top_precident) {
                         operators.push(i);
+                        operators.push(' ');
                         continue;
                     }
+
+                    // if the current operator has the same priority as the top operator, pop the top operator and push the current operator to the stack
                     if (current_precident == top_precident) {
                         outputs.push(operators.top());
+                        outputs.push(' ');
                         operators.pop();
                         operators.push(i);
+                        operators.push(' ');
                         continue;
                     }
+
+                    // if the current operator has a lower priority than the top operator, pop the top operator and push the current operator to the stack
                     if (current_precident < top_precident) {
                         while (!operators.empty()) {
                             outputs.push(operators.top());
+                            outputs.push(' ');
                             operators.pop();
                         }
                         operators.push(i);
@@ -95,32 +120,33 @@ int main(){
 
                 }
                 throw invalid_argument("Invalid input");
-            }
-            // TODO: check for multiple digit numbers
-            else{
+            }else{
                 // check for multiple digit numbers, if the next character is a digit, then continue
                 // if the next character is a digit, then continue
-                cout << "count: " << count << " its corresponding value is " << input[count] << endl;
-                char new_i =input[count-1];
-                cout << "i: " << i << " new_i "<< new_i<< endl;
-                int num = i - '0';
-                cout << "num: " << num << endl;
+                outputs.push(i);
+
+                while (input[count] >= '0' && input[count] <= '9'){
+                    outputs.push(input[count]);
+                    count++;
+                    index++;
+                }
+                outputs.push(' ');
+
             }
 
             prev = i;
-            outputs.push(i);
+
         } catch (const invalid_argument& e){
             cout << e.what() << endl;
         }
     }
 
-//    while (!ope.empty()){
-//
-//    }
+    // push the remaining operators to the outputs
     while (!operators.empty()){
         outputs.push(operators.top());
         operators.pop();
     }
+
 
     cout << "Result: " << calculate_resutl(outputs) << endl;
     return 0;
--- Homeworks/Week9/shunting_yard.cpp
+++ Homeworks/Week9/shunting_yard.cpp
@@ -10,26 +10,40 @@
 #include "shunting_yard.hpp"
 
 
-bool isOperator(char c){
-    return c == '+' || c == '-' || c == '*' || c == '/';
-}
 
-bool is_bracket(char c){
-    return c == '(' || c == ')';
-}
-int calculate_resutl(queue<char>& input){
+double calculate_resutl(queue<char>& input){
     // calculate the result from Queue: 5 9 8 * 2 / + 9 -
-    stack<int> numbers;
+    stack<double> numbers;
+
     while (!input.empty()){
         char c = input.front();
-        input.pop();
+
+        // if the character is a space, pop it and continue
+        if (c == ' '){
+            input.pop();
+            continue;
+        }
+
+        // if the character is a number, pop it and continue
         if (c >= '0' && c <= '9'){
-            numbers.push(c - '0');
+            input.pop();
+            // read input till you get the number before the space
+            // so that you can push the whole number not just the char to the stacks
+            int number = 0;
+            while (c >= '0' && c <= '9'){
+                number = number * 10 + c - '0';
+                c = input.front();
+                input.pop();
+            }
+
+            numbers.push(number);
         }
         else{
-            int a = numbers.top();
+            // if the character is an operator, pop it and apply the operation
+            input.pop();
+            double a = numbers.top();
             numbers.pop();
-            int b = numbers.top();
+            double b = numbers.top();
             numbers.pop();
             switch (c){
                 case '+':
@@ -50,6 +64,7 @@ int calculate_resutl(queue<char>& input){
     return numbers.top();
 }
 
+// check the priority of the operator and return the priority
 int priotity(char c){
     if (c == '+' || c == '-'){
         return 1;
@@ -60,6 +75,17 @@ int priotity(char c){
     return 0;
 }
 
+// check if the character is a letter
 bool is_letter(char c){
     return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z';
+}
+
+// check if the character is an operator
+bool isOperator(char c){
+    return c == '+' || c == '-' || c == '*' || c == '/';
+}
+
+// check if the character is a bracket
+bool is_bracket(char c){
+    return c == '(' || c == ')';
 }
