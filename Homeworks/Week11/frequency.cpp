b2e3ccc94f70cc88bcdb85a3844e27cf31ef4437
ShadowBurn1210 <leo.saulitis@gmail.com>
2024-03-30 16:03:38 +0200

    Just need to add wstring working

--- Homeworks/Week11/frequency.cpp
+++ Homeworks/Week11/frequency.cpp
@@ -10,10 +10,10 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 
-    map<wchar_t, int> frequency;
+    map<char, int> frequency;
     string input_file;
     string output_file = "not_found";
-    wstring line;
+    string line;
     bool lowercase = false;
     for (int i = 0; i < argc; i++) {
         if (strcmp(argv[i], "-i") == 0) {
@@ -24,7 +24,7 @@ int main(int argc, char *argv[]) {
             lowercase = true;
         }
     }
-    wifstream file(input_file);
+    ifstream file(input_file);
 
     if (!file.is_open()) {
         // cerr is used to print errors to the standard error stream
@@ -36,11 +36,10 @@ int main(int argc, char *argv[]) {
     locale::global(locale("C.UTF-8"));
 
     while (getline(file, line)) {
-        cout << "Input file: " << endl;
-        for (wchar_t i: line) {
-            if (lowercase) {
-                i = towlower(i);
-            }
+        for (auto i : line) {
+//            if (lowercase) {
+//                i = tolower(i);
+//            }
             if (isspace(i)) {
                 continue;
             }
@@ -52,14 +51,13 @@ int main(int argc, char *argv[]) {
         }
     }
     // add to frequency map test element
-    frequency[L'Ņ'] = 1;
     // write to output file with extension tsv
     if (output_file == "not_found") {
         // remove extension from input file
         output_file = input_file.substr(0, input_file.find_last_of("."));
         // add _frequency.tsv to the input file name
         output_file += ".tsv";
-        wofstream output(output_file);
+        ofstream output(output_file);
         for (auto const &[key, val]: frequency) {
             output << key << "\t" << val << endl;
         }
@@ -67,7 +65,7 @@ int main(int argc, char *argv[]) {
         if (output_file.find(".tsv") == string::npos) {
             output_file += ".tsv";
         }
-        wofstream output(output_file);
+        ofstream output(output_file);
         for (auto const &[key, val]: frequency) {
             output << key << "\t" << val << endl;
         }
